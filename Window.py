3d93e6fbd858f30dd21dc7c1a483da2d7218bb4f
RobbinBob <74874311+RobbinBob@users.noreply.github.com>
2022-03-08 23:03:08 +0000

    tab menu

--- Window.py
+++ Window.py
@@ -1,50 +1,52 @@
 #import tkinter
 from tkinter import *
-from tkinter import ttk # Newer widgets
+from tkinter import ttk
+from tokenize import String
+from turtle import st # Newer widgets
 
 #------------------FUNCTIONS------------------
-def calculate(*args):
-    try:
-        value = float(textEntry.get())
-        meters.set(int(0.3048 * value * 10000.0 + 0.5) / 10000.0)
-    except ValueError:
-        pass
+def updateChoice(*args):
+    value = funcs.get()
+    if value == 0:
+        print("Gradient line")
+    elif value == 1:
+        print("Gradient circle")
+    elif value == 2:
+        print("Gradient Spiral")
+    else:
+        print("Gradient idek how you got this")
 
 #-------------------WINDOW--------------------
 root = Tk()
 root.title('Gradient Support Designer')
 
+root.minsize(250, 400)
 ##  Create a frame to store the widgets in to theme it
-mainframe = ttk.Frame(root, padding='3 3 12 12')
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe = ttk.Frame(root)
+mainframe.grid(sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
+##  Apply dark theme to window
 root.tk.call('source', 'Azure-ttk-theme-2.1.0/azure.tcl')
 root.tk.call('set_theme', 'dark')
 
-##  Create entry widget
-textEntry = StringVar()
-textEntry_entry = ttk.Entry(mainframe, width=7, textvariable=textEntry)
-textEntry_entry.grid(column=2, row=1, sticky=(W, E))
+s = ttk.Style()
+##  Create radio buttons for different modes
+s.configure('SidebarBackground.TFrame', background='#242424')
+s.configure('SidebarBackground.TRadiobutton', background='#242424')
 
-##  Create label widget
-meters = StringVar()
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+funcframe = ttk.Frame(mainframe, padding=(3, 3, 12, 12), style='SidebarBackground.TFrame', width=250)
+funcframe.grid(row=0, column=0, sticky='nsew')
 
-##  Create button widget
-ttk.Button(mainframe, text='Calculate', command=calculate).grid(column=3, row=3, sticky=W)
+funcframe.grid_columnconfigure(0, weight=2)
+root.grid_columnconfigure(0, weight=1)
+mainframe.grid_rowconfigure(0, weight=1)
 
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-for child in mainframe.winfo_children():
-    child.grid_configure(padx=5, pady=5)
-textEntry_entry.focus()
-
-##  Bind the return key to calculate function
-root.bind('<Return>', calculate)
+funcs = IntVar()
+funcs_b1 = ttk.Radiobutton(funcframe, text='Gradient Line', command=updateChoice, variable=funcs, value=0, style='SidebarBackground.TRadiobutton').grid(column=0, row=0, sticky=W)
+funcs_b2 = ttk.Radiobutton(funcframe, text='Gradient Circle [Not supported]', command=updateChoice, variable=funcs, state=DISABLED, value=1, style='SidebarBackground.TRadiobutton').grid(row=1, sticky=W)
+funcs_b3 = ttk.Radiobutton(funcframe, text='Gradient Spiral [Not supported]', command=updateChoice, variable=funcs, state=DISABLED, value=2, style='SidebarBackground.TRadiobutton').grid(row=2, sticky=W)
 
 ## Start the event loop
 root.mainloop()
